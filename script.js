afe1be1caf185e333938c5cb7fdfe0460afe90e7
Sarthak <sarthak.d@simformsolutions.com>
2023-03-01 08:52:08 +0530

    Alternative of event duplication in JS file.

--- script.js
+++ script.js
@@ -11,32 +11,53 @@ var currentScrollPos = window.pageYOffset;
 }
 
 //scroll through JS on single page
-const homeLink = document.getElementById("home-l");
-const workLink = document.getElementById("work-l");
-const blogLink = document.getElementById("blog-l");
-const aboutLink = document.getElementById("about-l");
+// const homeLink = document.getElementById("home-l");
+// const workLink = document.getElementById("work-l");
+// const blogLink = document.getElementById("blog-l");
+// const aboutLink = document.getElementById("about-l");
 
 const homePageDiv = document.getElementById("home-p");
 const workPageDiv = document.getElementById("work-p");
 const blogPageDiv = document.getElementById("blog-p");
 const aboutPageDiv = document.getElementById("about-p");
 
-homeLink.addEventListener("click", e => {
-  e.preventDefault();
-  homePageDiv.scrollIntoView();
-});
+// homeLink.addEventListener("click", e => {
+//   e.preventDefault();
+//   homePageDiv.scrollIntoView();
+// });
 
-workLink.addEventListener("click", e => {
-  e.preventDefault();
-  workPageDiv.scrollIntoView();
-});
+// workLink.addEventListener("click", e => {
+//   e.preventDefault();
+//   workPageDiv.scrollIntoView();
+// });
 
-blogLink.addEventListener("click", e => {
-  e.preventDefault();
-  blogPageDiv.scrollIntoView();
-});
+// blogLink.addEventListener("click", e => {
+//   e.preventDefault();
+//   blogPageDiv.scrollIntoView();
+// });
 
-aboutLink.addEventListener("click", e => {
+// aboutLink.addEventListener("click", e => {
+//   e.preventDefault();
+//   aboutPageDiv.scrollIntoView();
+// });
+
+var clickCheck = document.getElementById("navbar");
+clickCheck.addEventListener('click', e => {
   e.preventDefault();
-  aboutPageDiv.scrollIntoView();
+  switch (e.target.id) {
+    case "home-l":
+      homePageDiv.scrollIntoView();
+      break;
+    case "work-l":
+      workPageDiv.scrollIntoView();
+      break;
+    case "blog-l":
+      blogPageDiv.scrollIntoView();
+      break;
+    case "about-l":
+      aboutPageDiv.scrollIntoView();
+      break;
+    default:
+      break;
+  }
 });
